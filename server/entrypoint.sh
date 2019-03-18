94ca994f5060543dbb3b1d06952c69f866ffc57d
Oliver Wegner <void1976@gmail.com>
2019-03-18 14:17:23 +0100

    insert masquerading iptables rule before starting openvpn server

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,4 +5,7 @@ if [ ! -e /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
+iptables -t nat -C POSTROUTING -s 10.255.255.0/24 -o eth0 -j MASQUERADE || \
+    iptables -t nat -I POSTROUTING -s 10.255.255.0/24 -o eth0 -j MASQUERADE
+
 openvpn --config /etc/openvpn/server.conf
