1da80b3fcc4eec3f0486f38826f963ce13696008
Oliver Wegner <void1976@gmail.com>
2019-03-19 12:48:49 +0100

    adding:
    - env vars via docker-compose
    - adding LOCAL_IPS config env var to DNAT certain IPs to docker host IP

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-mkdir -p /dev/net
-if [ ! -e /dev/net/tun ]; then
-    mknod /dev/net/tun c 10 200
-fi
+VPN_IPRANGE=${VPN_IPRANGE:-10.255.255.0/24}
+HOSTIP_DOCKER=$(ip route | awk '/^default/ { print $3; }')
 
-iptables -t nat -C POSTROUTING -s 10.255.255.0/24 -o eth0 -j MASQUERADE || \
-    iptables -t nat -I POSTROUTING -s 10.255.255.0/24 -o eth0 -j MASQUERADE
+function ensure_tun_device {
+    mkdir -p /dev/net
+    if [ ! -e /dev/net/tun ]; then
+        mknod /dev/net/tun c 10 200
+    fi
+}
 
-openvpn --config /etc/openvpn/server.conf
+function ensure_nat {
+    iptables -t nat -C POSTROUTING -s $VPN_IPRANGE -o eth0 -j MASQUERADE || \
+        iptables -t nat -I POSTROUTING -s $VPN_IPRANGE -o eth0 -j MASQUERADE
+
+    for LOCALIP in $(echo $LOCAL_IPS | sed -r -e 's/[[:space:]]+/ /g' | tr ' ' '\n'); do
+        iptables -t nat -C PREROUTING -i tun0 -s $VPN_IPRANGE -d $LOCALIP -j DNAT --to-destination $HOSTIP_DOCKER || \
+            iptables -t nat -I PREROUTING -i tun0 -s $VPN_IPRANGE -d $LOCALIP -j DNAT --to-destination $HOSTIP_DOCKER
+    done
+}
+
+function run {
+    openvpn --config /etc/openvpn/server.conf
+}
+
+ensure_tun_device
+ensure_nat
+run
